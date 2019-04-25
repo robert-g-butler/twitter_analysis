4d65821e3906f983eb384680c931bc6b5930125a
Robert Butler <mr.robert.g.butler@gmail.com>
2019-04-24 23:41:02 -0700

    Move database methods into DatabaseLoader class.

--- twitter_analyzer_tweepy.py
+++ twitter_analyzer_tweepy.py
@@ -3,21 +3,33 @@
 Created: 2019-04-09
 Author: Robert Butler
 Purpose: This is a script for streaming Twitter API data.
+
+TODO: Consider investigating the following Twitter users:
+    'AndrewYNg', 'rdpeng', 'CoreyMSchafer',
+    'raymondh', 'gvanrossum', 'dabeaz', 'nedbat', 'rOpenSci',  'rdpeng',
+    'xieyihui', 'SciPyTip', 'daattali', 'kdnuggets', 'rbloggers',
+    'RLangTip', 'R_Programming'
 """
 
-import textwrap
 import tweepy
 import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.sqlite import DATETIME
+from sqlalchemy.orm import sessionmaker
+
+import textwrap
 import plotly
 import plotly.offline as offl
 import plotly.graph_objs as go
-import sqlite3
-import sqlalchemy
 
 TWITTER_API_KEY = '5sOPB7UPox3jexWEYZ53C6kEJ'
 TWITTER_API_KEY_SECRET = 'G0CwqysE5nR1lFSNxQPKsb38UMLqh4UVxZWRrkZbxtdokNJxTe'
 TWITTER_ACCESS_TOKEN = '1115813594048815104-LklqugH11PQK7nlvkPRXhStbVwAHCt'
 TWITTER_ACCESS_TOKEN_SECRET = 'DAIPpCYIR9lPgtAtXw8fq4rSp0DmaAFq2mCcyPQFwFzXL'
+
+# Twitter Classes ------------------------------------------------------------
 
 class TwitterAuthenticator:
     '''
@@ -39,6 +51,7 @@ class TwitterListener(tweepy.StreamListener):
     '''
     def __init__(self, output_filename):
         self.output_filename = output_filename
+
     def on_data(self, data):
         '''Handles data storage for Twitter Streamer.'''
         try:
@@ -61,9 +74,11 @@ class TwitterClient:
         self.twitter_authenticator = TwitterAuthenticator()
         self.auth = self.twitter_authenticator.authenticate_twitter_conn()
         self.api = tweepy.API(auth_handler=self.auth)
+        
     def get_twitter_api(self):
         '''Returns the connected Twitter API.'''
         return self.api
+
     def get_remaining_api_calls(self):
         '''Returns the number of API calls left available for the hour.'''
         limit_status = self.api.rate_limit_status()
@@ -72,23 +87,27 @@ class TwitterClient:
         app_rls = app['/application/rate_limit_status']
         remaining = app_rls['remaining']
         return remaining
+
     def get_location_trends(self, loc_id=2487956):
         '''Returns the top 10 trends in an area.'''
         loc_trend_data = self.api.trends_place(id=loc_id)
         loc_trends = loc_trend_data[0]['trends']
         return loc_trends
+
     def get_user_timeline_tweets(self, user=None, limit=0):
         '''Returns a list of the user's most recently posted tweets.'''
         cursor = tweepy.Cursor(method=self.api.user_timeline,
                                id=user)
         tweets = cursor.items(limit=limit)
         return [t for t in tweets]
+
     def get_user_friends(self, user=None, limit=0):
         '''Returns a list of the user's friends.'''
         cursor = tweepy.Cursor(method=self.api.friends,
                                id=user)
         friends = cursor.items(limit=limit)
         return [f for f in friends]
+
     def get_home_timeline_tweets(self, limit=0):
         '''Returns a list of tweets on my home timeline.'''
         cursor = tweepy.Cursor(method=self.api.home_timeline)
@@ -100,6 +119,7 @@ class TwitterStreamer:
     def __init__(self):
         self.twitter_authenticator = TwitterAuthenticator()
         self.auth = self.twitter_authenticator.authenticate_twitter_conn()
+
     def stream_tweets(self, output_filename, filter_keywords):
         '''This handles connection to the Twitter API.'''
         listener = TwitterListener(output_filename)
@@ -111,116 +131,149 @@ class TwitterAnalyzer:
     @staticmethod
     def make_tweet_dataframe(tweets):
         '''Returns a pandas data frame with tweet data.'''
-        df = pd.DataFrame()
-        df['screen_name'] = pd.Series([t.author.screen_name for t in tweets])
-        df['id'] = pd.Series([t.id for t in tweets])
-        df['created_at'] = pd.Series([t.created_at for t in tweets])
-        df['source'] = pd.Series([t.source for t in tweets])
-        df['text'] = pd.Series([t.text for t in tweets])
-        df['favorite_count'] = pd.Series([t.favorite_count for t in tweets])
-        df['retweet_count'] = pd.Series([t.retweet_count for t in tweets])
-        return df
+        dataframe = pd.DataFrame()
+        dataframe['screen_name'] = pd.Series([t.author.screen_name
+                                              for t in tweets])
+        dataframe['id'] = pd.Series([t.id for t in tweets])
+        dataframe['created_at'] = pd.Series([t.created_at for t in tweets])
+        dataframe['source'] = pd.Series([t.source for t in tweets])
+        dataframe['text'] = pd.Series([t.text for t in tweets])
+        dataframe['favorite_count'] = pd.Series([t.favorite_count
+                                                 for t in tweets])
+        dataframe['retweet_count'] = pd.Series([t.retweet_count
+                                                for t in tweets])
+        dataframe = dataframe[
+            dataframe['text']
+            .apply(lambda t:
+                   not t.startswith('RT @') and not t.startswith('@'))
+        ]
+        return dataframe
+
     @staticmethod
     def make_trend_dataframe(trends):
         '''Returns a pandas data frame with tweet data.'''
-        df = pd.DataFrame()
-        df['name'] = pd.Series([t['name'] for t in trends])
-        df['url'] = pd.Series([t['url'] for t in trends])
-        df['tweet_volume'] = pd.Series([t['tweet_volume'] for t in trends])
-        return df
+        dataframe = pd.DataFrame()
+        dataframe['name'] = pd.Series([t['name'] for t in trends])
+        dataframe['url'] = pd.Series([t['url'] for t in trends])
+        dataframe['tweet_volume'] = pd.Series([t['tweet_volume']
+                                               for t in trends])
+        return dataframe
+
+# SQLAlchemy Classes ---------------------------------------------------------
+
+class DatabaseLoader:
+    '''This proivdes methods for populating the twitter database.'''
+    def __init__(self, db_path='sqlite:///:memory:'):
+        self.engine = sqlalchemy.create_engine(db_path)
+        self.base = declarative_base()
+        self.base.metadata.create_all(self.engine)
+        self.bound_session = sessionmaker(bind=self.engine)
+        self.session = self.bound_session()
+
+        self.client = TwitterClient()
+        self.api = self.client.get_twitter_api()
+
+    def refresh_tweets_table(self, users=('AndrewYNg',
+                                          'rdpeng',
+                                          'CoreyMSchafer')):
+        '''Empties and repopulates the 'tweets' table.'''
+        self.session.query(Tweets).delete()
+        for user in users:
+            tweets = self.client.get_user_timeline_tweets(user=user)
+            tweets_df = TwitterAnalyzer.make_tweet_dataframe(tweets=tweets)
+            mapped_tweets = [
+                Tweets(screen_name=row['screen_name'],
+                       tweet_id=row['id'],
+                       created_at=row['created_at'],
+                       source=row['source'],
+                       text=row['text'],
+                       favorite_count=row['favorite_count'],
+                       retweet_count=row['retweet_count'])
+                for index, row in tweets_df.iterrows()
+            ]
+            self.session.add_all(mapped_tweets)
+            self.session.commit()
+
+    def refresh_trends_table(self):
+        '''Empties and repopulates the 'trends' table.'''
+        self.session.query(Trends).delete()
+        trends = self.client.get_location_trends(loc_id=1)
+        trends_df = TwitterAnalyzer.make_trend_dataframe(trends=trends)
+        trends_df.dropna(inplace=True)
+        mapped_trends = [
+            Trends(name=row['name'],
+                   url=row['url'],
+                   tweet_volume=row['tweet_volume'])
+            for index, row in trends_df.iterrows()
+        ]
+        self.session.add_all(mapped_trends)
+        self.session.commit()
+
+        def get_remaining_api_calls(self):
+            return self.client.get_remaining_api_calls()
+
+DB_LOADER = DatabaseLoader(db_path='sqlite:///twitter_data.db')
+
+class Tweets(DB_LOADER.base):
+    '''This proves the model for the 'tweets' table.'''
+    __tablename__ = 'tweets'
+
+    id = Column(Integer, primary_key=True)
+    screen_name = Column(String)
+    tweet_id = Column(Integer)
+    created_at = Column(DATETIME)
+    source = Column(String)
+    text = Column(String)
+    favorite_count = Column(Integer)
+    retweet_count = Column(Integer)
+
+    def __repr__(self):
+        return (f'<Tweet(screen_name={self.screen_name}, '
+                f'created_at={self.created_at}, '
+                f'text={self.text})>')
+
+class Trends(DB_LOADER.base):
+    '''This proves the model for the 'trends' table.'''
+    __tablename__ = 'trends'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    url = Column(String)
+    tweet_volume = Column(Integer)
+
+    def __repr__(self):
+        return (f'<Trend(name={self.name}, '
+                f'url={self.url}, '
+                f'tweet_volume={self.tweet_volume})>')
+
+if __name__ == '__main__':
+    DB_LOADER.refresh_tweets_table()
+    DB_LOADER.refresh_trends_table()
 
 
-USERS = [
-    'CoreyMSchafer', 'raymondh', 'gvanrossum', 'dabeaz', 'nedbat',
-    'rbloggers', 'RLangTip', 'rOpenSci', 'rdpeng', 'xieyihui', 'SciPyTip',
-    'daattali', 'kdnuggets', 'R_Programming'
-]
-
-client = TwitterClient()
-api = client.get_twitter_api()
-client.get_remaining_api_calls()
-tweets = client.get_user_timeline_tweets(user=USERS[0])
-tweets_df = TwitterAnalyzer.make_tweet_dataframe(tweets=tweets)
-tweets_df = tweets_df[tweets_df['text']
-                      .apply(lambda t: not t.startswith('RT @'))]
-tweets_df.head()
-tweets_df.dtypes
-
-# TODO: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
-
-engine = sqlalchemy.create_engine('sqlite://', echo=False)
-
-tweets_df.to_sql(name='user_tweets', con=engine)
-
-sqlalchemy.sql.select(columns=[''])
-
-pd.DataFrame().to_sql()
 
 
 
 
 
 
+# for row in DB_LOADER.session.query(Tweets).all()[:5]:
+#     print(row)
 
+# USER = 'AndrewYNg'
 
+# sql_tweets_df = pd.read_sql(sql=f"SELECT * FROM tweets WHERE screen_name = '{USER}';", con=DB_LOADER.engine)
 
+# trace1 = go.Scatter({'x': sql_tweets_df['created_at'], 'y': sql_tweets_df['favorite_count'], 'mode': 'lines', 'name': 'Favorites', 'text': ['\n'.join(textwrap.wrap(t)) for t in sql_tweets_df['text']]})
+# #trace2 = go.Scatter({'x': sql_tweets_df['created_at'], 'y': sql_tweets_df['retweet_count'], 'mode': 'lines', 'name': 'Retweets'})
+# data = [trace1]
+# layout = go.Layout({'title': f'Posts from {USER}'})
+# offl.plot({'data': data, 'layout': layout})
 
-
-
-
-
-conn = sqlite3.connect(':memory:')
-c = conn.cursor()
-
-c.execute('CREATE TABLE IF NOT EXISTS twitter_user_timelines '
-             '(screen_name TEXT, id INTEGER, source TEXT, '
-             'text TEXT, favorite_count INTEGER, retweet_count INTEGER)')
-conn.commit()
-
-c.execute(
-    (
-        'INSERT INTO twitter_user_timelines '
-        '(screen_name, id, source, text, favorite_count, '
-        'retweet_count) '
-        'VALUES '
-        '(:screen_name, :id, :source, :text, :favorite_count, '
-        ':retweet_count)'
-    ),
-    {
-        'screen_name': tweets_df.at[0, 'screen_name'],
-        'id': tweets_df.at[0, 'id'],
-        #'created_at': tweets_df.at[0, 'created_at'],
-        'source': tweets_df.at[0, 'source'],
-        'text': tweets_df.at[0, 'text'],
-        'favorite_count': tweets_df.at[0, 'favorite_count'],
-        'retweet_count': tweets_df.at[0, 'retweet_count']
-    }
-)
-
-c.execute('SELECT * FROM twitter_user_timelines')
-[print(row) for row in c.fetchall()]
-
-c.close()
-conn.close()
-
-
-
-
-trace1 = go.Scatter({'x': tweets_df['created_at'], 'y': tweets_df['favorite_count'], 'mode': 'lines', 'name': 'Favorites', 'text': ['\n'.join(textwrap.wrap(t)) for t in tweets_df['text']]})
-trace2 = go.Scatter({'x': tweets_df['created_at'], 'y': tweets_df['retweet_count'], 'mode': 'lines', 'name': 'Retweets'})
-data = [trace1, trace2]
-layout = go.Layout({'title': f'Posts from {USER}'})
-offl.plot({'data': data, 'layout': layout})
-
-sf_trends = client.get_location_trends()
-sf_trends_df = TwitterAnalyzer.make_trend_dataframe(trends=sf_trends)
-sf_trends_df.dropna(inplace=True)
-sf_trends_df.head()
-
-trace1 = go.Bar({'x': sf_trends_df['tweet_volume'], 'y': sf_trends_df['name'], 'orientation': 'h', 'name': 'San Fransisco Trends', 'marker': {'color': '#ff7800', 'line': {'color': '#a54e00', 'width': 2}}})
-data = [trace1]
-layout = go.Layout({'title': 'Currently Trending on Twitter'})
-offl.plot({'data': data, 'layout': layout})
+# trace1 = go.Bar({'x': sf_trends_df['tweet_volume'], 'y': sf_trends_df['name'], 'orientation': 'h', 'name': 'San Fransisco Trends', 'marker': {'color': '#ff7800', 'line': {'color': '#a54e00', 'width': 2}}})
+# data = [trace1]
+# layout = go.Layout({'title': 'Currently Trending on Twitter'})
+# offl.plot({'data': data, 'layout': layout})
 
 
 
